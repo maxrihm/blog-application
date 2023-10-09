841cf09c9dcb59a5220d3b15cbcaf90ba092012a
maxrihm <morgenlatte2020@gmail.com>
2023-10-09 03:47:03 +0300

    Add ApplicationDbContext and related models

---
+++ UserService/Models/Role.cs
@@ -0,0 +1,10 @@
+﻿namespace UserService.Models
+{
+    public class Role
+    {
+        public int RoleId { get; set; }
+        public string Name { get; set; }
+        public ICollection<UserRole> UserRoles { get; set; }
+    }
+
+}
---
+++ UserService/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace UserService.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime DateCreated { get; set; }
+        public ICollection<UserRole> UserRoles { get; set; }
+    }
+}
