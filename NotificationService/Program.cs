b9d3f3992ecf6d362d4ac8c9e4be7b6b2960a4c6
maxrihm <morgenlatte2020@gmail.com>
2023-10-10 22:21:24 +0300

    Add SignalR service and RabbitMQ listener

--- NotificationService/Program.cs
+++ NotificationService/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.AspNetCore.SignalR;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Add SignalR service
+builder.Services.AddSignalR();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -20,6 +27,41 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+// Map the SignalR hub
+app.MapHub<NotificationHub>("/notificationHub");
+
 app.MapControllers();
 
+// RabbitMQ Listener
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "likeNotificationQueue",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    var hubContext = app.Services.GetService<IHubContext<NotificationHub>>();
+    hubContext.Clients.All.SendAsync("ReceiveMessage", "Server", message);
+};
+
+channel.BasicConsume(queue: "likeNotificationQueue",
+                     autoAck: true,
+                     consumer: consumer);
+
 app.Run();
+
+public class NotificationHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
