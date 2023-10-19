6b03eac22cb4e29cc91e8d2c39fadf03549cfc26
maxrihm <morgenlatte2020@gmail.com>
2023-10-19 22:26:35 +0300

    Refactor build-docker.bat to compile the React app and build a Docker image

--- frontend-react-blog/build-docker.bat
+++ frontend-react-blog/build-docker.bat
@@ -2,6 +2,11 @@
 
 REM Get the directory where the batch file is located
 set BATCH_PATH=%~dp0
+REM Change to the React app directory (assuming the batch file is located at the root of the React app)
+cd %BATCH_PATH%
+
+REM Compile the React app
+npm run build
 
 REM Stop and remove the existing Docker container (if it exists)
 docker stop frontend_container
@@ -9,12 +14,6 @@ docker rm frontend_container
 
 REM Delete the existing Docker image (if it exists)
 docker rmi frontend-react-app:latest -f
-
-REM Change to the React app directory (assuming the batch file is located at the root of the React app)
-cd %BATCH_PATH%
-
-REM Compile the React app
-npm run build
 
 REM Build the Docker image
 docker build -t frontend-react-app:latest .
--- frontend-react-blog/src/components/Post.js
+++ frontend-react-blog/src/components/Post.js
@@ -1,18 +1,31 @@
 // frontend-react-blog/src/components/Post.js 
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';  // <-- Import useNavigate hook
+import { useSelector } from 'react-redux';
 import { Card, CardContent, CardActionArea, Typography } from '@mui/material';
 
 const Post = ({ post }) => {
+  const navigate = useNavigate();  // <-- Get the navigate function
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+  
   const formattedDate = new Date(post.dateCreated).toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'long',
     day: 'numeric'
   });
 
+  const handleCardClick = () => {
+    if (!isLoggedIn) {
+      alert("Please log in to view the post.");
+      navigate('/login');  // <-- Use navigate function
+    } else {
+      navigate(`/post/${post.postId}`);  // <-- Use navigate function
+    }
+  };
+
   return (
     <Card className="post-card">
-      <CardActionArea component={Link} to={`/post/${post.postId}`}>
+      <CardActionArea onClick={handleCardClick}>
         <CardContent>
           <Typography variant="h6" component="div">
             {post.title}
@@ -29,7 +42,6 @@ const Post = ({ post }) => {
           <Typography variant="subtitle2" color="textSecondary" style={{ marginTop: '0.5rem' }}>
             Likes: {post.totalLikes}
           </Typography>
-          {/* Likes are omitted for now as per your instruction */}
         </CardContent>
       </CardActionArea>
     </Card>
